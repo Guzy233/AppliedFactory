e2476d33c2a89b52718d2e46d6c0f57e7cdbeb03
Guzy <114514>
2026-08-13 13:12:04 +0800

    增加彩灯播放逻辑

--- script-api/demov2.js
+++ script-api/demov2.js
@@ -4,22 +4,25 @@ let furnaces_input = [];
 
 let furnaces_output = [];
 
-function refreshFurnaces() {
+let lamps = [];
+
+function init() {
   furnaces_input = production.buses.filter(
-    (bus) =>
-      bus.target.id === "minecraft:furnace" &&
-      bus.targetFace === "up",
+    (bus) => bus.target.id === "minecraft:furnace" && bus.targetFace === "up",
   );
 
   furnaces_output = production.buses.filter(
-    (bus) =>
-      bus.target.id === "minecraft:furnace" &&
-      bus.targetFace === "down",
+    (bus) => bus.target.id === "minecraft:furnace" && bus.targetFace === "down",
   );
+
+  lamps = production.buses
+    .filter((bus) => bus.target.id === "minecraft:redstone_lamp")
+    .sort((a, b) => a.target.x - b.target.x);
+
 }
 
-production.onChange(refreshFurnaces);
-refreshFurnaces();
+production.onChange(init);
+init();
 
 registerProcessingPattern(
   [
@@ -40,11 +43,16 @@ go(function* () {
     // Non-blocking variants are explicit.
     // const remaining = source.extract(item("minecraft:coal", -1)).to(target).now();
     for (var bus of furnaces_output) {
-        var output = bus.extract();
-        if (output.length > 0) {
-            output.to(main).now();
-            log("something returned!");
+      var output = bus.extract();
+      if (output.length > 0) {
+        output.to(main).now();
+        log("something returned!");
+        for (var lamp of lamps) {
+          lamp.redstone(15);
+          yield sleep(2);
+          lamp.redstone(0);
         }
+      }
     }
     yield sleep(30);
   }
