1159812b404862953edd96d20cd0beda1d076330
Guzy <114514>
2026-08-13 08:01:23 +0800

    修复生产脚本按面过滤

--- script-api/demov2.js
+++ script-api/demov2.js
@@ -1,13 +1,22 @@
 const production = network("east");
 const main = network("west");
-let furnaces = [];
+let furnaces_input = [];
+
+let furnaces_output = [];
 
 function refreshFurnaces() {
-  furnaces = production.buses.filter(
+  furnaces_input = production.buses.filter(
     (bus) =>
       bus.target !== null &&
       bus.target.id === "minecraft:furnace" &&
       bus.targetFace === "up",
+  );
+
+  furnaces_output = production.buses.filter(
+    (bus) =>
+      bus.target !== null &&
+      bus.target.id === "minecraft:furnace" &&
+      bus.targetFace === "down",
   );
 }
 
@@ -25,7 +34,7 @@ registerProcessingPattern(
   ],
   function* (order) {
     // EXACT waits for all source resources and all destination capacity.
-    yield order.input.pushExactlyInto(furnaces[0]);
+    yield order.input.pushExactlyInto(furnaces_input[0]);
   },
 );
 
@@ -33,7 +42,7 @@ go(function* () {
   while (true) {
     // Non-blocking variants are explicit.
     // const remaining = source.extract(item("minecraft:coal", -1)).to(target).now();
-    for (var bus of production.buses) {
+    for (var bus of furnaces_output) {
       bus.extract().to(main).now();
     }
     yield sleep(20);
